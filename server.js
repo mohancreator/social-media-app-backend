f0f13be119b34b9547e7f257978e8a6834c299eb
Mohan <madhiazagan2002@gmail.com>
2024-11-02 12:45:34 +0530

    Prepare backend for Vercel deployment

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');  // Import the CORS package
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 const app = express();
 connectDB();
+
+// Enable CORS for requests from http://localhost:3000
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow these HTTP methods
+  credentials: true  // Enable credentials if necessary
+}));
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
