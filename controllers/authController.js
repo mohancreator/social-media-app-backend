605e5816e0c19c675525f1564985ae53c156eef1
Mohan <madhiazagan2002@gmail.com>
2024-11-02 12:12:14 +0530

    Backend Files

---
+++ controllers/authController.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.signup = async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ username, email, password: hashedPassword });
+        res.status(201).json({ message: 'User created', user });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return res.status(404).json({ message: 'User not found' });
+        
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+        res.json({ token, user });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
