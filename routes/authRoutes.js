62158687a21c08aeb6b75b1135a87c5570aabded
Mohan <madhiazagan2002@gmail.com>
2024-11-02 12:58:57 +0530

    Prepare backend for Vercel deployment

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
+// authRoutes.js
 const express = require('express');
 const { signup, login } = require('../controllers/authController');
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post('/register', signup);
 router.post('/login', login);
 
 module.exports = router;
