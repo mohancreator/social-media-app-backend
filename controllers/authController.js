2969b6749c49622189949949f8d59084b63fd487
Mohan <madhiazagan2002@gmail.com>
2024-11-02 17:33:39 +0530

    Prepare backend for Vercel deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,25 +16,16 @@ exports.signup = async (req, res) => {
 exports.login = async (req, res) => {
     const { email, password } = req.body;
     try {
-        console.log("Starting login process for:", email); // Log email
         const user = await User.findOne({ email });
-        if (!user) {
-            console.log("User not found");
-            return res.status(404).json({ message: 'User not found' });
-        }
+        if (!user) return res.status(404).json({ message: 'User not found' });
         
         const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            console.log("Password mismatch");
-            return res.status(400).json({ message: 'Invalid credentials' });
-        }
+        if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
-        console.log("Token created successfully:", token);
         res.json({ token, user });
     } catch (error) {
-        console.error("Error during login:", error); // Log the error details
+        console.error("Login error:", error); // Log detailed error message
         res.status(500).json({ error: error.message });
     }
 };
-
